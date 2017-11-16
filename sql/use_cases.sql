4f15426b5ca1eeaf967c02c00eb1e8e0abf1bbcf
David Navarro <navarro.david.j@gmail.com>
2017-11-16 15:52:15 -0800

    Some membership relatated use cases

--- sql/use_cases.sql
+++ sql/use_cases.sql
@@ -1,3 +1,31 @@
+
+# Sign up for GameGo membership
+
+# Check GameGo Membership award points
+SELECT points 
+FROM users NATURAL JOIN memberships 
+WHERE email = "testertim@gmail.com";
+
+# Use GameGo Membership award points
+
+# Modifying member / membership
+
+# End membership
+
+# Search up member
+SELECT * 
+FROM users NATURAL JOIN memberships 
+WHERE email = "testertim@gmail.com";
+
+SELECT * 
+FROM users NATURAL JOIN memberships 
+WHERE name = "Tester Tim";
+
+SELECT * 
+FROM users NATURAL JOIN memberships 
+WHERE mid = "0";
+
+
 #Buy item
 
 #Rent/Loan item
