bda07581d150c7cd5c01f24280101e99c0cc2380
Luigi Lantin <luigilantin@gmail.com>
2017-11-17 17:38:11 -0800

    added my statements

--- sql/use_cases.sql
+++ sql/use_cases.sql
@@ -51,3 +51,62 @@ WHERE mid = "0";
 #Search by title genre, rating, price, console_type
 
 #Search overdue items
+
+
+#Count preorders
+SELECT COUNT(*)
+FROM Preorders;
+
+#update a title, genre, rating, price, etc
+UPDATE Games
+SET title = 'this tittle'
+WHERE gid = 0;
+
+UPDATE Games
+SET author = 'this guy'
+WHERE gid = 0;
+
+UPDATE Games
+SET genre = FPS
+WHERE gid = 0;
+
+UPDATE Games
+SET rating = 5
+WHERE gid = 0;
+
+UPDATE Games
+SET price = 1.00
+WHERE gid = 0;
+
+UPDATE Games
+SET stock = 1
+WHERE gid = 0;
+
+
+UPDATE Games
+SET title = 'this tittle', 
+author = 'this guy', 
+genre = FPS,
+rating = 5,
+price = 1.00,
+stock = 1
+WHERE gid = 0;
+
+#find item's rental history
+SELECT *
+FROM Rentals
+WHERE gid = 1;
+#check specific person's purchase history
+SELECT *
+FROM Transactions
+WHERE uid = 1;
+
+#add/remove item on sale
+INSERT INTO Sales(gid, discount)
+VALUES(1, .25);
+
+#promote user to admin
+INSERT INTO Admins(uid, name, email, password)
+SELECT uid, name, email, password
+FROM Users
+WHERE uid = 1;
