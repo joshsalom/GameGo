33ca6c9897f5a63cbb0e5ca4cbdf9ebf2675758d
Joshua S <joshua.salom1996@gmail.com>
2017-11-19 12:47:35 -0800

    modified triggers: trigger script works

--- sql/triggers.sql
+++ sql/triggers.sql
@@ -1,21 +1,40 @@
-CREATE TRIGGER trgRentalInsert
+### see all triggers of gamego
+-- select trigger_schema, trigger_name, action_statement from information_schema.triggers where information_schema.triggers.trigger_schema like '%gamego%';
+
+USE GAMEGO;
+
+DROP TRIGGER IF EXISTS afterInsertRentalDec;
+DELIMITER //
+CREATE TRIGGER afterInsertRentalDec
 AFTER INSERT ON Rentals
 FOR EACH ROW    
+BEGIN
     UPDATE games
     SET stock = stock - 1
     WHERE games.gid = NEW.gid;
-    
-CREATE TRIGGER trgTransactionGame
-AFTER INSERT ON transactions
-FOR EACH ROW	
-    UPDATE games
-    SET stock = stock - 1
-    WHERE games.gid = NEW.gid;
+END//
+DELIMITER ;
 
-CREATE TRIGGER trgTransactionConsole
+DROP TRIGGER IF EXISTS afterInsertTransactionGameDec;
+DELIMITER //
+CREATE TRIGGER afterInsertTransactionGameDec
 AFTER INSERT ON transactions
 FOR EACH ROW	
+BEGIN
+    UPDATE games
+    SET stock = stock - 1
+    WHERE games.gid = NEW.gid;
+END//
+DELIMITER ;
+
+DROP TRIGGER IF EXISTS afterInsertTransactionConsoleDec;
+DELIMITER //
+CREATE TRIGGER afterInsertTransactionConsoleDec
+AFTER INSERT ON transactions
+FOR EACH ROW	
+BEGIN
     UPDATE consoles
     SET stock = stock - 1
     WHERE consoles.cid = NEW.cid;
-    
+END//
+DELIMITER ;   
